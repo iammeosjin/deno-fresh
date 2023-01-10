c67289d18bcafd854b6b3969f1fe75d1cbbc9e10
iammeosjin <iammeosjin@gmail.com>
2023-01-10 18:10:58 +0800

    fix: fix component header

--- routes/reservation.tsx
+++ routes/reservation.tsx
@@ -7,6 +7,9 @@ import { getCookies } from 'std/http/cookie.ts';
 import ReservationStepper from '../islands/Reservation-Stepper.tsx';
 import { ComponentChildren } from 'preact';
 import { useContext } from 'preact/hooks';
+import { h } from 'preact';
+
+/** @tsx h */
 
 export const handler: Handlers<Context> = {
 	async GET(req, ctx) {
@@ -36,6 +39,17 @@ export default function Home(
 ) {
 	const data = props.data || {};
 
+	let context: ComponentChildren[];
+	try {
+		context = useContext(HEAD_CONTEXT);
+	} catch (err) {
+		throw new Error(
+			'<Head> component is not supported in the browser, or during suspense renders.',
+			{ cause: err },
+		);
+	}
+	context.push(props.children);
+
 	return (
 		<>
 			<title>
@@ -46,7 +60,7 @@ export default function Home(
 				<link rel='stylesheet' href='css/output.css' />
 				<link rel='stylesheet' href='css/common.css' />
 				<NavBar user={data.user} path={data.path} />
-				{/* <ReservationStepper spot={undefined} /> */}
+				<ReservationStepper spot={undefined} />
 				<script src='js/flowbite.js' />
 			</body>
 		</>

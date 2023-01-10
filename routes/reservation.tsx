54c9983b51b28adcfe682d7386b8b234a8862a80
iammeosjin <iammeosjin@gmail.com>
2023-01-10 18:02:15 +0800

    fix: set reservation context

--- routes/reservation.tsx
+++ routes/reservation.tsx
@@ -1,10 +1,12 @@
 import { Handlers, PageProps } from '$fresh/server.ts';
-import { Head } from '$fresh/runtime.ts';
+import { Head, HEAD_CONTEXT } from '$fresh/runtime.ts';
 import NavBar from '../components/NavBar.tsx';
 import { Account, Context } from '../type.ts';
 import AccountModel from '../models/account.ts';
 import { getCookies } from 'std/http/cookie.ts';
 import ReservationStepper from '../islands/Reservation-Stepper.tsx';
+import { ComponentChildren } from 'preact';
+import { useContext } from 'preact/hooks';
 
 export const handler: Handlers<Context> = {
 	async GET(req, ctx) {
@@ -29,8 +31,21 @@ export const handler: Handlers<Context> = {
 	},
 };
 
-export default function Home({ data }: PageProps<Context>) {
-	const props = data || {};
+export default function Home(
+	props: PageProps<Context> & { children: ComponentChildren },
+) {
+	const data = props.data || {};
+
+	let context: ComponentChildren[];
+	try {
+		context = useContext(HEAD_CONTEXT);
+	} catch (err) {
+		throw new Error(
+			'<Head> component is not supported in the browser, or during suspense renders.',
+			{ cause: err },
+		);
+	}
+	context.push(props.children);
 
 	return (
 		<>
@@ -41,7 +56,7 @@ export default function Home({ data }: PageProps<Context>) {
 			<body>
 				<link rel='stylesheet' href='css/output.css' />
 				<link rel='stylesheet' href='css/common.css' />
-				<NavBar user={props.user} path={props.path} />
+				<NavBar user={data.user} path={data.path} />
 				<ReservationStepper spot={undefined} />
 				<script src='js/flowbite.js' />
 			</body>

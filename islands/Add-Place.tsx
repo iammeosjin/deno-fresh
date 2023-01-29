be0710f6df771df852d7baa6ae3fd96714d40e38
iammeosjin <iammeosjin@gmail.com>
2023-01-29 12:47:42 +0800

    refactor: remove description limit

--- islands/Add-Place.tsx
+++ islands/Add-Place.tsx
@@ -393,7 +393,6 @@ export default function AddPlace() {
 														placeholder='Description'
 														required={false}
 														autoComplete='off'
-														maxLength={256}
 														class='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
 													/>
 												</div>
